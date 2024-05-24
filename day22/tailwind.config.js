fcebd57240d36a0e4973c4e4b0b308f161b2db65
fungnumalimbu <168177493+fungnumalimbu@users.noreply.github.com>
2024-05-24 17:22:45 +0545

    tailwind css

--- day22/tailwind.config.js
+++ day22/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'jumpy': 'jump 2s ease-in infinite',
+      },
+      keyframes: {
+        jump: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(3git0px)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
